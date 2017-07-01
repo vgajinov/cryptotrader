a1e969f1db043e3b0a0ac06b1668db6c0e72ac79
Felix Rubio <felix@kngnt.org>
2017-07-01 22:29:08 +0200

    Added SQLite support
    * Created a class to save quotes data and to load it from file, to prevent spending bandwidth
    * Modifying, not finished, the algorithm to detect local maximums and minimums

--- test.py
+++ test.py
@@ -1,14 +1,103 @@
 #!/usr/bin/env python2
 
-import cryptowatch.Client as cl
-import time
-from datetime import datetime
-import matplotlib.pyplot as plt
-from matplotlib.finance import candlestick_ohlc
-from matplotlib.dates import date2num, DateFormatter, HourLocator, DayLocator
 from finance_metrics import *
+import sys
+
+class dbconnector(object):
+
+   def __init__(self, fname):
+      self.con, self._db_is_initialized = self.connect_to_db(fname)
+
+   @property
+   def db_is_initialized(self):
+      return self._db_is_initialized
+
+   def __enter__(self):
+      return self
+
+   def __exit__(self, exc_type, exc_value, traceback):
+         self.close_db()
+
+   def connect_to_db(self, fname):
+      import sqlite3 as lite
+   
+      con = None
+   
+      # connect to the database, will be created if not existent
+      try:
+         con = lite.connect(fname)
+         c = con.cursor()
+      except lite.Error, e:
+         print "Error %s:" % e.args[0]
+         sys.exit(1)
+   
+      sql_create_data_table = """ CREATE TABLE IF NOT EXISTS samples (
+                                          ts real PRIMARY KEY,
+                                          open real,
+                                          high real,
+                                          low real,
+                                          close real,
+                                          volume real
+                                      ); """
+   
+      # create the table for the data, if it does not exist
+      try:
+         c.execute(sql_create_data_table)
+      except Error as e:
+         print(e)
+         sys.exit(1)
+   
+      # get the number of rows (to find if db is initialized or not)
+      try:
+         c.execute('SELECT Count(*) FROM samples')
+         data = c.fetchone()[0]
+      except Error as e:
+         print(e)
+         sys.exit(1)
+   
+      # return the conection, and a boolean indicating if there is data in the database
+      return con, data > 0
+
+   def close_db(self):
+      if self.con:
+         self.con.close()
+
+   def save_data(self, quotes):
+      cur = self.con.cursor()
+      cur.executemany("INSERT INTO samples VALUES(?, ?, ?, ?, ?, ?)", map(tuple, quotes.tolist()))
+      self.con.commit()
+   
+   def load_data(self):
+      cur = self.con.cursor()
+   
+      # get the number of rows
+      try:
+         cur.execute('SELECT Count(*) FROM samples')
+         nrows = cur.fetchone()[0]
+      except Error as e:
+         print(e)
+         sys.exit(1)
+   
+      quotes = np.ndarray(shape=(nrows,6), dtype=float)
+   
+      try:
+         cur.execute('SELECT * FROM samples')
+         records = cur.fetchall()
+      except Error as e:
+         print(e)
+         sys.exit(1)
+   
+      for idx,record in enumerate(records):
+         quotes[idx][:] = record
+   
+      return quotes
+
 
 def display_plot(quotes, technical_indicators=None, processing_results=None):
+   import matplotlib.pyplot as plt
+   from matplotlib.finance import candlestick_ohlc
+   from matplotlib.dates import date2num, DateFormatter, HourLocator, DayLocator
+
    # plot the candle chart
    # set the formatters for the horizontal axis
    days = DayLocator()
@@ -75,15 +164,15 @@ def get_trade_proposals(quotes, technical_indicators):
    #for elem in technical_indicators['psar_bear']:
    #   print elem
 
-   local_mins = [ i for i, j in enumerate(technical_indicators['psar_bear']) if j is None and technical_indicators['psar_bear'][i-1] is not None]
+   local_mins = [ min(i+consec_nones, numsamples) for i, j in enumerate(technical_indicators['psar_bear']) if j is None and technical_indicators['psar_bear'][i-1] is not None]
 
    # restrict the eligible elements only to those that have AT LEAST N consecutives Nones
    buy_time_idx = []
    for i in local_mins:
-      window = min(consec_nones, numsamples - i)
+      window = max(i - consec_nones, 0 )
    
-      if all([sample is None for sample in technical_indicators['psar_bear'][i:i+window]]):
-         buy_time_idx.append(min(i+window,numsamples-1))
+      if all([sample is None for sample in technical_indicators['psar_bear'][window:i]]):
+         buy_time_idx.append(i)
    
    # get the indices of the None elements in array, whose next element is None
    local_maxs = [ i for i, j in enumerate(technical_indicators['psar_bull']) if j is None and technical_indicators['psar_bull'][i-1] is not None]
@@ -91,14 +180,15 @@ def get_trade_proposals(quotes, technical_indicators):
    # restrict the eligible elements only to those that have AT LEAST N consecutives Nones
    sell_time_idx = []
    for i in local_maxs:
-      window = min(consec_nones, numsamples - i)
+      window = max(i-consec_nones, 0)
    
-      if all([sample is None for sample in technical_indicators['psar_bull'][i:i+window]]):
-         sell_time_idx.append(min(i+window,numsamples-1))
+      if all([sample is None for sample in technical_indicators['psar_bull'][window:i]]):
+         sell_time_idx.append(i)
 
    # generate an order book
    OrderBook = [ (quotes[i,0], 'B', quotes[i,1]) for i in buy_time_idx ] + [ (quotes[i,0], 'S', quotes[i,1]) for i in sell_time_idx ]
-   
+  
+   print OrderBook
    return sorted(OrderBook, key=lambda x: x[0])
 
 
@@ -138,13 +228,28 @@ def simulate_trading(OrderBook):
    
 
 period='1800'
-s = "26/06/2017 00:00:00"
-today=int(time.mktime(datetime.strptime(s, "%d/%m/%Y %H:%M:%S").timetuple()))
 
-client = cl.MarketClient("kraken", "etheur")
-OHLC = client.GetOHLC(after=str(today), periods=[period])
+with dbconnector( "samples_%s_26062017_000000.db"%period ) as dbObj:
+   if not dbObj.db_is_initialized:
+      from matplotlib.dates import date2num
+      import cryptowatch.Client as cl
+      import time
+      from datetime import datetime
+   
+      s = "26/06/2017 00:00:00"
+      after=int(time.mktime(datetime.strptime(s, "%d/%m/%Y %H:%M:%S").timetuple()))
+      #s = "27/06/2017 00:00:00"
+      #before=int(time.mktime(datetime.strptime(s, "%d/%m/%Y %H:%M:%S").timetuple()))
+   
+      client = cl.MarketClient("kraken", "etheur")
+      #OHLC = client.GetOHLC(before=str(before), after=str(after), periods=[period])
+      OHLC = client.GetOHLC( after=str(after), periods=[period])
+   
+      quotes = np.array( [ (date2num(datetime.fromtimestamp(x.open_time)), x.open, x.high, x.low, x.close, x.volume) for x in OHLC[period] ], dtype=float )
+      dbObj.save_data(quotes)
+   else:
+      quotes = dbObj.load_data()
 
-quotes = np.array( [ (date2num(datetime.fromtimestamp(x.open_time)), x.open, x.high, x.low, x.close, x.volume) for x in OHLC[period] ], dtype=float )
 #technical_indicators = KELCH(quotes, 14)
 technical_indicators = {}
 
