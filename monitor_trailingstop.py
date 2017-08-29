fed02c666db8a6dacb667ca8c52561d95fe32208
Felix Rubio <felix@kngnt.org>
2017-08-29 23:12:41 +0200

    Added sellMkt order when trailing stop is crossed

--- monitor_trailingstop.py
+++ monitor_trailingstop.py
@@ -202,7 +202,10 @@ if __name__ == "__main__":
             stop_loss[idx] = midprice - stop_loss_distance[mkt][cryptocurr]
             logger.debug("TS reset for crypto {} in market {} at relative price {:.3f}: {:.3f}".format(cryptocurr, mkt, midprice, stop_loss[idx] ))
          elif midprice < stop_loss[idx]:
-            logger.info("ALERT TS for crypto {} in market {} at relative price {:.3f}: {:.3f}".format(cryptocurr, mkt, midprice,  ))
+            msg = "TS triggered for crypto {} in market {} at relative price {:.3f}: {:.3f}".format(cryptocurr, mkt, midprice, stop_loss[idx] )
+            logger.info(msg)
+            mkt.sellMkt(cryptocurr, vol)
+            logger.notify(msg)
             alert_trailing_stop[idx] = True
 
          # check the volume-related data for this cryptocurrency (if it has not been alread
