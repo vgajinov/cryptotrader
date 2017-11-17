d0c521f46c2537370112bd46438385262fb97197
Vladimir <vladimir@stalker.srb>
2017-11-17 23:16:36 +0100

    added parabolic SAR overlay

--- guiCandleChart.py
+++ guiCandleChart.py
@@ -3,38 +3,10 @@ from pydispatch import dispatcher
 import pyqtgraph as pg
 from PyQt5 import QtCore, QtGui, QtWidgets, QtChart
 import exchanges.bitfinex.bitfinex_v2_WebSockets as bitfinexWS
-import math
-
-
-# timestamp,  open,  high,  low,   close
-ohclData = [
-[1435708800000, 126.90, 126.94, 125.9, 126.60],
-[1435795200000, 126.69, 126.69, 126.6, 126.69],
-[1436140800000, 124.85, 126.23, 124.8, 126.00],
-[1436227200000, 125.89, 126.15, 123.7, 125.69],
-[1436313600000, 124.64, 124.64, 122.5, 122.54],
-[1436400000000, 123.85, 124.06, 119.2, 120.07],
-[1436486400000, 121.94, 123.85, 121.2, 123.28],
-[1436745600000, 125.03, 125.76, 124.3, 125.66],
-[1436832000000, 126.04, 126.37, 125.0, 125.61],
-[1436918400000, 125.72, 127.15, 125.5, 126.82],
-[1437004800000, 127.74, 128.57, 127.3, 128.51],
-[1437091200000, 129.08, 129.62, 128.3, 129.62],
-[1437350400000, 130.97, 132.97, 130.7, 132.07],
-[1437436800000, 132.85, 132.92, 130.3, 130.75],
-[1437523200000, 121.99, 125.50, 121.9, 125.22],
-[1437609600000, 126.20, 127.09, 125.0, 125.16],
-[1437696000000, 125.32, 125.74, 123.9, 124.50],
-[1437955200000, 123.09, 123.61, 122.1, 122.77],
-[1438041600000, 123.38, 123.91, 122.5, 123.38],
-[1438128000000, 123.15, 123.50, 122.2, 122.99],
-[1438214400000, 122.32, 122.57, 121.7, 122.37],
-[1438300800000, 122.60, 122.64, 120.9, 121.30]
-]
 
 
 
-class ChartWidget(QtWidgets.QWidget):
+class CandleChart(QtChart.QChart):
    candleData = None
    numCandlesVisible = 50
    minCandlesVisible = 20
@@ -45,33 +17,43 @@ class ChartWidget(QtWidgets.QWidget):
    currTimeframeIndex = 0
 
    def __init__(self):
-      super(ChartWidget, self).__init__()
+      super(CandleChart, self).__init__()
 
-      self.mainLayout = QtWidgets.QVBoxLayout(self)
-
-      self.candleGraph = QtChart.QChart()
-      self.candleGraph.legend().setVisible(True)
-      self.candleGraph.legend().setAlignment(QtCore.Qt.AlignBottom)
-      self.candleGraph.setBackgroundBrush(QtGui.QBrush(QtGui.QColor(0,0,0)))
-      self.candleGraph.setBackgroundPen(QtGui.QPen(QtGui.QColor(255, 80, 80), 0.5))
-      self.candleGraph.setBackgroundRoundness(0)
-      self.candleGraph.setContentsMargins(0,0,0,0)
-      self.candleGraph.setWindowFrameMargins(0,0,0,0)
-      self.candleGraph.legend().hide()
-      chartFont = QtGui.QFont(self.candleGraph.font())
+      self.legend().setVisible(True)
+      self.legend().setAlignment(QtCore.Qt.AlignBottom)
+      self.setBackgroundBrush(QtGui.QBrush(QtGui.QColor(0,0,0)))
+      self.setBackgroundPen(QtGui.QPen(QtGui.QColor(255, 80, 80), 0.5))
+      self.setBackgroundRoundness(0)
+      self.setContentsMargins(0,0,0,0)
+      self.setWindowFrameMargins(0,0,0,0)
+      self.legend().hide()
+      chartFont = QtGui.QFont(self.font())
       chartFont.setPixelSize(9)
-      self.candleGraph.setFont(chartFont)
-
-
-      chartView = QtChart.QChartView(self.candleGraph)
-      chartView.setRenderHint(QtGui.QPainter.Antialiasing)
-
-      self.mainLayout.addWidget(chartView)
+      self.setFont(chartFont)
+      self.setAcceptHoverEvents(True)
 
       self.candlestickSeries = QtChart.QCandlestickSeries()
       self.candlestickSeries.setIncreasingColor(QtCore.Qt.black)
       self.candlestickSeries.setDecreasingColor(QtCore.Qt.red)
-      self.candleGraph.addSeries(self.candlestickSeries)
+      self.addSeries(self.candlestickSeries)
+
+      self.hoverLinePriceTag = QtWidgets.QGraphicsSimpleTextItem(self)
+      self.hoverLinePriceTag.setBrush(QtGui.QBrush(QtGui.QColor(255,255,255)))
+      self.hoverLine = QtChart.QLineSeries()
+      hoverPen = QtGui.QPen(QtCore.Qt.DashLine)
+      hoverPen.setColor(QtCore.Qt.white)
+      hoverPen.setWidth(0)
+      hoverPen.setDashPattern([5,10])
+      self.hoverLine.setPen(hoverPen)
+      self.hoverLineAxisX = QtChart.QValueAxis()
+      self.hoverLineAxisX.hide()
+      self.hoverLineAxisX.setRange(0,1)
+      self.addSeries(self.hoverLine)
+      self.addAxis(self.hoverLineAxisX, QtCore.Qt.AlignBottom)
+      self.hoverLine.attachAxis(self.hoverLineAxisX)
+
+      self.addOverlays()
+
 
 
 
@@ -79,65 +61,12 @@ class ChartWidget(QtWidgets.QWidget):
       self.candleData = data
       self.updateCandleChart()
 
-      # if self.candlestickSeries.count() == 0:
-      #    data = data[-self.numCandlesVisible:]
-      #    self.candlestickSetList = [QtChart.QCandlestickSet(x[1], x[2], x[3], x[4], timestamp=x[0]) for x in data]
-      #    for set in self.candlestickSetList:
-      #       self.setCandleColors(set)
-      #
-      #    self.candlestickSeries.append(self.candlestickSetList)
-      #    self.candleGraph.addSeries(self.candlestickSeries)
-      #    self.updateCandleChart()
-      #
-      # else:
-      #    # update only the last candlestick item
-      #    item = data[-1]
-      #    newSet = QtChart.QCandlestickSet(item[1], item[2], item[3], item[4], timestamp=item[0])
-      #    lastSet = self.candlestickSeries.sets()[-1]
-      #    self.setCandleColors(newSet)
-      #
-      #    if newSet.timestamp() == lastSet.timestamp():
-      #       # update
-      #       self.candlestickSeries.remove(lastSet)
-      #       self.candlestickSeries.append(newSet)
-      #    else:
-      #       # new candle
-      #       self.candlestickSeries.remove(self.candlestickSeries.sets()[0])
-      #       self.candlestickSeries.append(newSet)
-      #       self.updateCandleChart()
-
-
 
    def updateCandleChart(self):
       data = self.candleData[-self.numCandlesVisible:]
       linuxTimestamps = [x[0]/1000 for x in data]
       timestamps = [QtCore.QDateTime.fromMSecsSinceEpoch(x[0]).toString('HH:mm') for x in data]
       categories = self.extractNiceCategories(linuxTimestamps)
-
-      ax = QtChart.QBarCategoryAxis()
-      ax.setCategories([QtCore.QDateTime.fromMSecsSinceEpoch(x[0]).toString('HH:mm') for x in data])
-      ax.setLinePen(QtGui.QPen(QtGui.QColor(0, 0, 0), 0.5))
-      ax.setLabelsAngle(90)
-      ax.setGridLineVisible(False)
-      ax.hide()
-
-      ax2 = QtChart.QCategoryAxis()
-      ax2.setLabelsPosition(QtChart.QCategoryAxis.AxisLabelsPositionOnValue)
-      for tst in categories:
-         ax2.append(tst, timestamps.index(tst))
-      ax2.setLinePen(QtGui.QPen(QtGui.QColor(0, 0, 0), 0.5))
-      ax2.setGridLineVisible(False)
-      ax2.setStartValue(-1)
-
-      maxVal = max([x[2] for x in data])
-      minVal = min([x[3] for x in data])
-      ay = QtChart.QValueAxis()
-      ay.setGridLinePen(QtGui.QPen(QtGui.QColor(80, 80, 80), 0.5))
-      ay.setLinePen(QtGui.QPen(QtGui.QColor(0, 0, 0), 0.5))
-      ay.setMax(maxVal)
-      ay.setMin(minVal)
-      ay.applyNiceNumbers()
-
 
       if self.candlestickSeries.count() > 0:
          self.candlestickSeries.remove(self.candlestickSeries.sets())
@@ -147,17 +76,54 @@ class ChartWidget(QtWidgets.QWidget):
          self.setCandleColors(set)
       self.candlestickSeries.append(self.candlestickSetList)
 
+      axisXtime = QtChart.QBarCategoryAxis()
+      axisXtime.setCategories([QtCore.QDateTime.fromMSecsSinceEpoch(x[0]).toString('HH:mm') for x in data])
+      axisXtime.setGridLineVisible(False)
+      axisXtime.hide()
+
+      self.axisXvalue = QtChart.QValueAxis()
+      self.axisXvalue.setRange(0, len(axisXtime))
+      self.axisXvalue.setGridLineVisible(False)
+      self.axisXvalue.hide()
+
+
+      axisXticks = QtChart.QCategoryAxis()
+      axisXticks.setLabelsPosition(QtChart.QCategoryAxis.AxisLabelsPositionOnValue)
+      for tst in categories:
+         axisXticks.append(tst, timestamps.index(tst))
+      axisXticks.setLinePen(QtGui.QPen(QtGui.QColor(0, 0, 0), 0.5))
+      axisXticks.setGridLineVisible(False)
+      axisXticks.setStartValue(-1)
+
+      maxVal = max([x[2] for x in data])
+      minVal = min([x[3] for x in data])
+      self.ay = QtChart.QValueAxis()
+      self.ay.setGridLinePen(QtGui.QPen(QtGui.QColor(80, 80, 80), 0.5))
+      self.ay.setLinePen(QtGui.QPen(QtGui.QColor(0, 0, 0), 0.5))
+      self.ay.setMax(maxVal)
+      self.ay.setMin(minVal)
+      self.ay.applyNiceNumbers()
+
       for axis in self.candlestickSeries.attachedAxes():
          self.candlestickSeries.detachAxis(axis)
-         self.candleGraph.removeAxis(axis)
+         self.removeAxis(axis)
 
-      self.candleGraph.addAxis(ax, QtCore.Qt.AlignTop)
-      self.candleGraph.addAxis(ax2, QtCore.Qt.AlignBottom)
-      self.candleGraph.addAxis(ay, QtCore.Qt.AlignRight)
+      self.addAxis(axisXtime, QtCore.Qt.AlignTop)
+      self.addAxis(self.axisXvalue, QtCore.Qt.AlignBottom)
+      self.addAxis(axisXticks, QtCore.Qt.AlignBottom)
+      self.addAxis(self.ay, QtCore.Qt.AlignRight)
 
-      self.candlestickSeries.attachAxis(ax)
-      self.candlestickSeries.attachAxis(ax2)
-      self.candlestickSeries.attachAxis(ay)
+      self.candlestickSeries.attachAxis(axisXtime)
+      self.candlestickSeries.attachAxis(axisXticks)
+      self.candlestickSeries.attachAxis(self.ay)
+
+      for axis in self.hoverLine.attachedAxes():
+         self.hoverLine.detachAxis(axis)
+      self.hoverLine.attachAxis(self.ay)
+
+      self.updateOverlays()
+
+
 
 
    def extractNiceCategories(self, timestamps):
@@ -169,7 +135,6 @@ class ChartWidget(QtWidgets.QWidget):
             break
       categories = [QtCore.QDateTime.fromSecsSinceEpoch(t).toString('HH:mm') for t in timestamps]
       return categories
-      #return [tst for tst in timestamps if tst[-1] == '0' or tst[-1] == '5']
 
 
    def setCandleColors(self, candleSet : QtChart.QCandlestickSet):
@@ -182,7 +147,7 @@ class ChartWidget(QtWidgets.QWidget):
 
 
    def wheelEvent(self, QWheelEvent):
-      if QWheelEvent.angleDelta().y() < 0:
+      if QWheelEvent.delta() < 0:
          # wheel down - zoom out
          self.numCandlesVisible = min(self.numCandlesVisible + 10, self.maxCandlesVisible)
          self.updateCandleChart()
@@ -191,6 +156,124 @@ class ChartWidget(QtWidgets.QWidget):
          self.numCandlesVisible = max(self.numCandlesVisible - 10, self.minCandlesVisible)
          self.currTimeframeIndex = max(1, self.currTimeframeIndex - 1)
          self.updateCandleChart()
+
+
+   def hoverMoveEvent(self, event : QtWidgets.QGraphicsSceneHoverEvent):
+      event.setAccepted(True)
+      pos = self.mapToParent(event.pos())
+      val = self.mapToValue(pos)
+      self.hoverLine.clear()
+      self.hoverLine.append(0, val.y())
+      self.hoverLine.append(1, val.y())
+      self.hoverLinePriceTag.setPos(self.plotArea().getCoords()[2] + 5, pos.y() - 7)  # TODO calculate offsets instead of hardcoding
+      self.hoverLinePriceTag.setText('{:.2f}'.format(val.y()))
+      yAxes = self.hoverLine.attachedAxes()[0]
+      if val.y() > yAxes.min() and val.y() < yAxes.max():
+         self.hoverLinePriceTag.show()
+      else:
+         self.hoverLinePriceTag.hide()
+
+   def hoverLeaveEvent(self, event : QtWidgets.QGraphicsSceneHoverEvent):
+      self.hoverLine.hide()
+      self.hoverLinePriceTag.hide()
+
+   def hoverEnterEvent(self, event: QtWidgets.QGraphicsSceneHoverEvent):
+      self.hoverLine.show()
+
+#=======================================================================================
+
+
+   def addOverlays(self):
+      self.psarOverlay = QtChart.QScatterSeries()
+      self.psarOverlay.setMarkerSize(1)
+      self.addSeries(self.psarOverlay)
+
+
+   def updateOverlays(self):
+      if not self.candleData:
+         return
+      self.psarOverlay.clear()
+      self.psarOverlay.attachAxis(self.axisXvalue)
+      self.psarOverlay.attachAxis(self.ay)
+      psarValues = self.parabolicSAR(self.candleData)
+      psarValues = psarValues[-self.numCandlesVisible:]
+      for i in range(self.numCandlesVisible):
+         self.psarOverlay.append(i+0.5, psarValues[i])
+
+
+   def parabolicSAR(self, data, iaf=0.02, maxaf=0.2):
+      length = len(data)
+      high = [x[2] for x in data]
+      low = [x[3] for x in data]
+      close = [x[4] for x in data]
+      psar = close[0:len(close)]
+
+      bull = True
+      af = iaf
+      ep = low[0]
+      hp = high[0]
+      lp = low[0]
+      for i in range(2, length):
+         if bull:
+            psar[i] = psar[i - 1] + af * (hp - psar[i - 1])
+         else:
+            psar[i] = psar[i - 1] + af * (lp - psar[i - 1])
+         reverse = False
+         if bull:
+            if low[i] < psar[i]:
+               bull = False
+               reverse = True
+               psar[i] = hp
+               lp = low[i]
+               af = iaf
+         else:
+            if high[i] > psar[i]:
+               bull = True
+               reverse = True
+               psar[i] = lp
+               hp = high[i]
+               af = iaf
+         if not reverse:
+            if bull:
+               if high[i] > hp:
+                  hp = high[i]
+                  af = min(af + iaf, maxaf)
+               if low[i - 1] < psar[i]:
+                  psar[i] = low[i - 1]
+               if low[i - 2] < psar[i]:
+                  psar[i] = low[i - 2]
+            else:
+               if low[i] < lp:
+                  lp = low[i]
+                  af = min(af + iaf, maxaf)
+               if high[i - 1] > psar[i]:
+                  psar[i] = high[i - 1]
+               if high[i - 2] > psar[i]:
+                  psar[i] = high[i - 2]
+
+      return psar
+
+
+
+#=======================================================================================
+
+
+
+class ChartWidget(QtWidgets.QWidget):
+   def __init__(self):
+      super(ChartWidget, self).__init__()
+
+      self.mainLayout = QtWidgets.QVBoxLayout(self)
+
+      self.candleGraph = CandleChart()
+      chartView = QtChart.QChartView(self.candleGraph)
+      chartView.setRenderHint(QtGui.QPainter.Antialiasing)
+      self.mainLayout.addWidget(chartView)
+
+   def setCandlestickData(self, data):
+      self.candleGraph.setCandlestickData(data)
+
+
 
 
 class CandlesUpdateEvent(QtCore.QEvent):
@@ -226,6 +309,7 @@ class MainWindow(QtGui.QMainWindow):
 
 
 if __name__ == '__main__':
+   QtGui.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling);
    app = QtGui.QApplication(sys.argv)
    GUI = MainWindow(800, 400)
    GUI.show()
